29ad8338b8bcaa7323364e89de0637982271332a
Joshua Ventura <jventura11@hawkmail.hccfl.edu>
2020-03-07 19:06:34 -0500

    Added some relations between tables

--- Rush/src/main/java/com/rush/model/Order.java
+++ Rush/src/main/java/com/rush/model/Order.java
@@ -1,5 +1,7 @@
 package com.rush.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -19,9 +22,6 @@ public class Order {
 	@Column(name="order_id")
 	private Long orderId;
 	
-	@Column(name="cust_id")
-	private Long cust_id;
-	
 	@Column(name="order_date")
 	private String order_date;
 
@@ -29,46 +29,9 @@ public class Order {
 	@JoinColumn(name="cust_id")
 	private Customer customer;
 	
+	@OneToMany
+	@JoinColumn(name="order_id")
+	private List<OrderItem> orderItems;
 	//Getters and Setters
-	public Long getOrderId() {
-		return orderId;
-	}
-
-	public void setOrderId(Long orderId) {
-		this.orderId = orderId;
-	}
-
-	public Long getCust_id() {
-		return cust_id;
-	}
-
-	public void setCust_id(Long cust_id) {
-		this.cust_id = cust_id;
-	}
-
-	public String getOrder_date() {
-		return order_date;
-	}
-
-	public void setOrder_date(String order_date) {
-		this.order_date = order_date;
-	}
-
-	@Override
-	public String toString() {
-		return "Order [orderId=" + orderId + ", cust_id=" + cust_id + ", order_date=" + order_date + "]";
-	}
-
-	public Order(Long orderId, Long cust_id, String order_date) {
-		super();
-		this.orderId = orderId;
-		this.cust_id = cust_id;
-		this.order_date = order_date;
-	}
-
-	public Order() {
-
-	}
-
 	
 }
