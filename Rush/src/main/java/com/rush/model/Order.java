2446edc59bc77e00727c9d6ea575d589c238e040
Khanh <knguye15.ccp@gmail.com>
2020-03-15 15:55:01 -0400

    order timestamp added

--- Rush/src/main/java/com/rush/model/Order.java
+++ Rush/src/main/java/com/rush/model/Order.java
@@ -1,5 +1,6 @@
 package com.rush.model;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -14,6 +15,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.CreationTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -38,7 +40,8 @@ public class Order {
 	@Column(name="order_date")
 	//@JsonView(DataView.OrderView.class)
 	@JsonView(DataView.ItemView.class)
-	private String order_date;
+	@CreationTimestamp
+	private Timestamp order_date;
 
 	@Column(name="order_total")
 	//@JsonView(DataView.OrderView.class)
@@ -62,11 +65,11 @@ public class Order {
 		this.orderId = orderId;
 	}
 
-	public String getOrder_date() {
+	public Timestamp getOrder_date() {
 		return order_date;
 	}
 
-	public void setOrder_date(String order_date) {
+	public void setOrder_date(Timestamp order_date) {
 		this.order_date = order_date;
 	}
 
@@ -94,7 +97,7 @@ public class Order {
 		this.orderItems = orderItems;
 	}
 
-	public Order(Long orderId, String order_date, Double orderTotal, Customer customer, List<OrderItem> orderItems) {
+	public Order(Long orderId, Timestamp order_date, Double orderTotal, Customer customer, List<OrderItem> orderItems) {
 		super();
 		this.orderId = orderId;
 		this.order_date = order_date;
