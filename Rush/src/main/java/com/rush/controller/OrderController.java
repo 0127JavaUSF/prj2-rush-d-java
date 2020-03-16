a897730ac73091d9814e84af7f169b3bf63ebe77
charley43613 <41710701+charley43613@users.noreply.github.com>
2020-03-15 21:55:16 -0400

    added authentication to order items

--- Rush/src/main/java/com/rush/controller/OrderController.java
+++ Rush/src/main/java/com/rush/controller/OrderController.java
@@ -1,10 +1,14 @@
 package com.rush.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +22,7 @@ import com.rush.jview.DataView;
 import com.rush.model.Order;
 import com.rush.model.OrderItem;
 import com.rush.repos.OrderRepo;
+import com.rush.utils.CustomerJWTUtil;
 //http://localhost:4200
 @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "true")
 @RestController
@@ -31,11 +36,29 @@ public class OrderController {
 	private OrderRepo orderRepo;
 	
 	@PostMapping("/orders")
-	public Order createOrder(@RequestBody Order order) {
-		logger.info("Saving Order");
-		System.out.println("Contents of the order being submitted" + order.toString());
+	public Order createOrder(@RequestBody Order order, @CookieValue(value = "JWT", defaultValue="noCookie") String jwt) {
 		
-		return orderRepo.save(order);
+		
+		try {
+			if (!(CustomerJWTUtil.getCustIdOrNull(jwt).equals(null))) {
+				logger.info("Saving Order");
+				System.out.println("Contents of the order being submitted" + order.toString());
+				
+				return orderRepo.save(order);
+				
+			}
+		}
+		catch(NullPointerException e) {
+			logger.error("Fail Seesion Initialization");	
+		}
+		return new Order();
+//	    return ResponseEntity
+//				.status(401)
+//				.body("JWT token value invalid");//return the payload for client to utilize the userId for the session
+//
+//		
+		
+
 	}
 	
 	@GetMapping("/orders/customer/{id}")
